3c1336a0cab1629308e841d09c2815b505606cbc
charbelMansourDev <130573508+charbelMansourDev@users.noreply.github.com>
2023-12-02 20:38:29 +0200

    .

--- utils/firebaseHelper.js
+++ utils/firebaseHelper.js
@@ -1,21 +1,7 @@
 import { initializeApp } from "firebase/app"
-// import { initializeAuth, getReactNativePersistence } from "firebase/auth"
-// import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
-
-// export const auth = initializeAuth(app, {
-//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-// })
 
 export const getFireBaseApp = () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCD20SedXLdSd0ADbkb0RJMDTOyMIjv6IA",
-    authDomain: "whatsapp-e1078.firebaseapp.com",
-    projectId: "whatsapp-e1078",
-    storageBucket: "whatsapp-e1078.appspot.com",
-    messagingSenderId: "182752442537",
-    appId: "1:182752442537:web:9782b8500f9748963bd1b8",
-    measurementId: "G-M78ZY0HD9Q",
-  }
+  const firebaseConfig = {}
 
   return initializeApp(firebaseConfig)
 }
