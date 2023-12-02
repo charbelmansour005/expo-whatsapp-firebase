3df2986264f0afa2c48afae23f579b985804d366
charbelMansourDev <130573508+charbelMansourDev@users.noreply.github.com>
2023-12-03 00:06:19 +0200

    help em out

--- utils/firebaseHelper.js
+++ utils/firebaseHelper.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app"
 import { initializeAuth, getReactNativePersistence } from "firebase/auth"
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
+// the import below (import firebaseConfig from "./firebaseConfig") would look like the following:
+// export default firebaseConfig = {
+//   apiKey: "",
+//   authDomain:  "",
+//   projectId:  "",
+//   storageBucket:  "",
+//   messagingSenderId: "",
+//   appId:  "",
+//   measurementId:  "",
+// }
 import firebaseConfig from "./firebaseConfig"
 
 const app = initializeApp(firebaseConfig)
